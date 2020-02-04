878680c45e4dd84d18f5a1756c580f6e05b6ba8a
ronscotty <33847911+ronscotty@users.noreply.github.com>
2020-02-04 15:32:39 -0800

    changes

--- test_pdf.py
+++ test_pdf.py
@@ -3,3 +3,6 @@ import PyPDF2
 pdfFileObj = open('tax_disclosure.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 print(pdfReader.numPages)
+print('contents: ')
+pageObj = pdfReader.getPage(0)
+print(pageObj.extractText())
